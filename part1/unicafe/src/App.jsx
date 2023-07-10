afa15d2bc5ceab359cc603aebf41c582ed7423dd
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-09 22:12:47 -0300

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,14 +4,15 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Feedback = ({ text, count }) => (
-  <p>{text} {count}</p>
+const Feedback = ({ text, count, percent }) => (
+  <p>{text} {count} {percent && '%'}</p>
 )
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
 
   return (
     <div>
@@ -24,6 +25,9 @@ const App = () => {
       <Feedback text='good' count={good} />
       <Feedback text='neutral' count={neutral} />
       <Feedback text='bad' count={bad} />
+      <Feedback text='all' count={total} />
+      <Feedback text='average' count={good - bad} />
+      <Feedback text='positive' count={(good * 100) / (total)} percent />
     </div>
   )
 }
