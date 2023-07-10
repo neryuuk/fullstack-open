68466bd9959672ea508d27e11199d8e4aae6c50e
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-10 16:26:38 -0300

    2.3*: Course information step8

--- part2/courseinfo/src/components/Sum.jsx
+++ part2/courseinfo/src/components/Sum.jsx
@@ -1,5 +1,5 @@
 const Sum = ({ parts }) => (
-  <strong>total of 42 exercises</strong>
+  <strong>total of {parts.reduce((sum, { exercises }) => sum + exercises, 0)} exercises</strong>
 )
 
 export default Sum
