12ade043bc07c71b400860f44bb28941af79e909
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-09 22:20:49 -0300

    1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
@@ -8,11 +8,24 @@ const Feedback = ({ text, count, percent }) => (
   <p>{text} {count} {percent && '%'}</p>
 )
 
+const Statistics = ({ good, bad, neutral }) => {
+  const total = good + neutral + bad
+
+  return <>
+    <h1>statistics</h1>
+    <Feedback text='good' count={good} />
+    <Feedback text='neutral' count={neutral} />
+    <Feedback text='bad' count={bad} />
+    <Feedback text='all' count={total} />
+    <Feedback text='average' count={good - bad} />
+    <Feedback text='positive' count={(good * 100) / (total)} percent />
+  </>
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
 
   return (
     <div>
@@ -21,13 +34,7 @@ const App = () => {
       <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
       <Button text='bad' handleClick={() => setBad(bad + 1)} />
 
-      <h1>statistics</h1>
-      <Feedback text='good' count={good} />
-      <Feedback text='neutral' count={neutral} />
-      <Feedback text='bad' count={bad} />
-      <Feedback text='all' count={total} />
-      <Feedback text='average' count={good - bad} />
-      <Feedback text='positive' count={(good * 100) / (total)} percent />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
