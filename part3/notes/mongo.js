dbbe0eb2a48dddd21ddbcd4bc30d6580c9027897
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-14 11:14:43 -0300

    Notes mongoose

---
+++ part3/notes/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const collection = 'noteApp'
+
+const url = `mongodb+srv://fullstack:${password}@fullstack-open.lgzchww.mongodb.net/${collection}?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  important: true,
+})
+
+/*
+note.save().then(_ => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
