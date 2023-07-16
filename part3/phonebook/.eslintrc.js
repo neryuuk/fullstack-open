6409dd8d2294ceb876ed2f73a5408c68e9d46f0d
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-16 17:21:15 -0300

    line-endings fix

--- part3/phonebook/.eslintrc.js
+++ part3/phonebook/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   'rules': {
     'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
     'eqeqeq': 'error',
