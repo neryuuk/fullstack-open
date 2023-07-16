c3ef6e34b7220f55a5da7b336e3f0ae1f01a1367
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-16 00:32:12 -0300

    Notes error handling

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -9,10 +9,6 @@ const Note = require('./models/notes')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-const unknown = (_, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
@@ -22,9 +18,7 @@ app.get('/', (_, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.route('/api/notes').post((request, response) => {
-  const { body } = request
-
+app.route('/api/notes').post(({ body }, response) => {
   if (!body.content) return response.status(400).json({ error: 'content is missing' })
 
   const note = new Note({
@@ -32,30 +26,44 @@ app.route('/api/notes').post((request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(result => {
-    response.json(result)
-  })
+  note.save().then(result => response.json(result))
 }).get((_, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+  Note.find({}).then(notes => response.json(notes))
 })
 
-app.route('/api/notes/:id').get((request, response, next) => {
-  Note.findById(request.params.id).then(note => {
+app.route('/api/notes/:id').get(({ params }, response, next) => {
+  Note.findById(params.id).then(note => {
     if (note) response.json(note)
     else next()
-  })
-}).delete((request, response) => {
-  const { id } = request.params
-  Note.findByIdAndDelete(id).then(() => {
-    response.status(204).end()
-  }).catch(error => {
-    response.status(400).json({ error: error.message })
-  })
+  }).catch(error => next(error))
+}).put(({ body, params }, response, next) => {
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(params.id, note, { new: true }).then(updated => {
+    response.json(updated)
+  }).catch(error => next(error))
+}).delete(({ params }, response, next) => {
+  Note.findByIdAndRemove(params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
 })
 
-app.use(unknown)
+app.use((error, _, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformed Id' })
+  }
+
+  next(error)
+})
+
+app.use((_, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+})
 
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGODB_URI).then((_) => {
