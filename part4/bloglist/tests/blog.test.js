a9948ff1a4e72570fd13c56202912af5d607c791
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-17 16:21:18 -0300

    4.4: helper functions and unit tests, step2

--- part4/bloglist/tests/blog.test.js
+++
@@ -1,5 +0,0 @@
-const { dummy } = require('../utils/list_helper')
-
-test('dummy returns one', () => {
-  expect(dummy([])).toBe(1)
-})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,4 +2,9 @@ const dummy = (blogs) => {
   return 1 || blogs.length
 }
 
-module.exports = { dummy }
+const totalLikes = (posts) => {
+  if (!posts || !posts.length) return 0
+  return posts.reduce((total, { likes }) => total + likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
