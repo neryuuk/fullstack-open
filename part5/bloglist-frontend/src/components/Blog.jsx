74f8b8c4a62963977aa11abfe8ba567f5ee92ed5
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-27 18:30:46 -0300

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,1 +1,14 @@
-export const Blog = ({ blog }) => <li>{blog.title} {blog.author}</li>
+import { useState } from 'react'
+
+export const Blog = ({ blog }) => {
+  const [complete, setComplete] = useState(false)
+
+  return <li><p>
+      {blog.title} {blog.author} <button onClick={() => setComplete(!complete)}>{complete ? 'hide' : 'view'}</button>
+      {complete && <span>
+        <br /><a href={blog.url}>{blog.url}</a>
+        <br />likes {blog.likes} <button>like</button>
+        <br />{blog?.user?.name}
+      </span>}
+    </p></li>
+}
