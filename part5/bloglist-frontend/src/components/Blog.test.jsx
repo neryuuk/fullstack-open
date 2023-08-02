db01cd1d9e1d6ed7421ba891a6e0e5e810b5a3df
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-02 14:55:14 -0300

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import { Blog } from './Blog'
 
 describe('<Blog />', () => {
-  test('renders content', () => {
+  let container
+
+  beforeEach(() => {
     const blog = {
       id: '64ba9b35a9cea045d48a6913',
       title: 'A Blog Title',
@@ -19,17 +21,28 @@ describe('<Blog />', () => {
       },
     }
 
-    const { container } = render(<Blog {...{
+    container = render(<Blog {...{
       blog,
       user: blog.user,
       handleLike: () => { },
       handleDelete: () => { },
-    }} />)
+    }} />).container
+  })
 
+  test('renders content', () => {
     const element = screen.getByText('A Blog Title Author McAuthor', { exact: false })
     expect(element).toBeDefined()
     expect(element).toHaveTextContent('A Blog Title Author McAuthor')
     expect(container.querySelector('blog-url')).toBe(null)
     expect(container.querySelector('blog-likes')).toBe(null)
+  })
+
+  test('display url and likes upon click', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(container.querySelector('blog-url')).toBeDefined()
+    expect(container.querySelector('blog-likes')).toBeDefined()
   })
 })
