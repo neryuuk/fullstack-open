d7110c830105112ce374886530e59e221e131e6d
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-03 16:51:59 -0300

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,41 @@
+const USER = {
+  name: 'Nelson A',
+  username: 'nelson',
+  password: '$lL018U5&xX32sDDh8RrTVdf',
+}
+
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', `${Cypress.env('BACKEND')}/tests/reset`)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, USER)
     cy.visit('')
   })
 
-  it('Login form is shown', function () {
+  it('login form is shown', function () {
     cy.contains('log in to application')
     cy.get('input#username')
     cy.get('input#password')
     cy.get('button#login-button')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('log in to application')
+      cy.get('input#username').type(USER.username)
+      cy.get('input#password').type(USER.password)
+      cy.get('button#login-button').click()
+      cy.contains(`${USER.name} logged in`)
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in to application')
+      cy.get('input#username').type(USER.username)
+      cy.get('input#password').type('password')
+      cy.get('button#login-button').click()
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+    })
   })
 })
