b4ee14075e32d822e6e365714f86fed6a92688cc
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-22 18:23:03 -0300

    Uncontrolled form

--- notes/redux-notes/src/index.jsx
+++ notes/redux-notes/src/index.jsx
@@ -2,33 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import noteReducer from './reducers/noteReducer'
+import './index.css'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: false,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: true,
-    id: 2,
-  },
-})
+const generateId = () => Number((Math.random() * 1000).toFixed(0))
 
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id },
+    })
+  }
+
   return <div>
+    <form onSubmit={addNote}>
+      <input name="note" />
+      <button type="submit">add</button>
+    </form>
     <ul>
-      {store.getState().map(note => <li key={note.id}>
-        {note.content} <strong>{note.important ? 'important' : ''}</strong>
-      </li>)}
+      {store.getState().map(note => (
+        <li key={note.id} onClick={() => toggleImportance(note.id)}>
+          {note.content} <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+      ))}
     </ul>
   </div>
 }
